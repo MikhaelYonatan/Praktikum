3b00031a439570d0662c3c08d97718c9c3f1dd7c
MikhaelYonatan <mikhael.yonatan10@gmail.com>
2025-10-17 14:32:03 +0700

    Menambahkan praktikum 5

---
+++ IF2110-Algoritma dan Pemrograman 2/Praktikum/Praktikum 5/SlidingWindow.hs
@@ -0,0 +1,78 @@
+module SlidingWindow where
+
+konso:: a -> [a] -> [a]
+konso e l = e : l
+
+konsDot:: [a] -> a -> [a]
+konsDot l e = l ++ [e]
+
+
+isEmpty:: [a] -> Bool
+isEmpty l = null l
+
+nbElement:: [a] -> Int
+nbElement l = if isEmpty l then 0 else 1 + nbElement (tail l)
+
+{-|
+  TakeN menerima sebuah integer n dan sebuah list l, dan menghasilkan sebuah list yang berisi n elemen pertama dari l. Jika n lebih besar dari panjang l, maka dihasilkan l itu sendiri.
+  Prekondisi: n >= 0, l bisa kosong.
+-}
+takeN:: Int -> [a] -> [a]
+-- TODO
+takeN n l
+    | isEmpty l = l
+    | n > length l = l
+    | n == 1 = [head l]
+    | otherwise =
+        konso (head l) (takeN (n-1) (tail l))
+
+{-|
+  Deskripsi:
+  Fungsi slidingWindow menerima sebuah integer ukuran window n prekondisi : 0 < n <= panjang xs,
+  sebuah fungsi pemroses f, dan sebuah list xs, prekondisi : panjang xs > 0. Fungsi ini akan
+  menghasilkan sebuah list baru yang berisi hasil dari penerapan
+  fungsi f ke setiap window dengan ukuran n. Dapat menggunakan fungsi takeN.
+
+  Contoh:
+  slidingWindow 3 windowRange [1, 2, 3, 4, 5] akan menghasilkan [2, 2, 2]
+-}
+slidingWindow :: Int -> ([a] -> b) -> [a] -> [b]
+-- TODO
+slidingWindow n f xs =
+    if (n == length xs) then
+        [f xs]
+    else
+        konso (f (takeN n xs)) (slidingWindow n f (tail(xs)))
+
+    
+
+{-|
+  Deskripsi:
+  Fungsi windowRange menghasilkan selisih antara nilai maksimum dan nilai minimum dari list tersebut.
+  nilai minimum dari list tersebut.
+
+  Contoh:
+  windowRange [1, 2, 3, 4, 5] akan menghasilkan 4
+-}
+windowRange:: [Int] -> Int
+maksimum :: [Int] -> Int
+minimini:: [Int] -> Int
+
+maksimum l
+    | isEmpty (tail l) = head l
+    | otherwise =
+        let
+            maks = max (head l) (maksimum(tail l))
+        in
+            maks
+
+minimini l
+    | isEmpty (tail l) = head l
+    | otherwise =
+        let
+            mins = min (head l) (minimini(tail l))
+        in
+            mins
+
+-- TODO
+windowRange l = maksimum l - minimini l
